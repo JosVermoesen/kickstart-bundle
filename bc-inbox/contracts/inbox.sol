6730b6d749ce5dba9e655e2062f399ef1e700704
Jos Vermoesen <info@rv.be>
2022-12-05 16:56:46 +0100

    cleanup contracts

--- bc-inbox/contracts/inbox.sol
+++ bc-inbox/contracts/inbox.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.4.26;
 
-// pragma solidity ^0.8.14;
-
 contract Inbox {
     string public message;
 
@@ -10,16 +8,7 @@ contract Inbox {
         message = initialMessage;
     }
 
-    // 0.8.14 change above to:
-    /* constructor(string memory initialMessage) {
-        message = initialMessage;
-    } */
-
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-    // 0.8.14 change above to:
-    /* function setMessage(string memory newMessage) public {
-        message = newMessage;
-    } */
 }
--- bc-lottery/contracts/lottery.sol
+++ bc-lottery/contracts/lottery.sol
@@ -1,17 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.4.26;
 
-// pragma solidity ^0.8.14;
-
 contract Lottery {
     address public manager;
-    // 0.8.14 change below to: address payable[] public players;
     address[] public players;
 
-    // 0.8.14 change below to:
-    /* constructor() {
-        manager = msg.sender;
-    } */
     constructor() public {
         manager = msg.sender;
     }
@@ -19,7 +12,6 @@ contract Lottery {
     // In Remix use Gwei 11000000 for sending 0.011 ether
     function enter() public payable {
         require(msg.value > .01 ether);
-        // 0.8.14 change below to: players.push(payable(msg.sender));
         players.push(msg.sender);
     }
 
@@ -35,7 +27,6 @@ contract Lottery {
     function pickWinner() public restricted {
         uint256 index = random() % players.length;
         players[index].transfer(address(this).balance);
-        // 0.8.14 change below to: players = new address payable[](0);
         players = new address[](0);
     }
 
@@ -44,10 +35,6 @@ contract Lottery {
         _;
     }
 
-    // 0.8.14 change below to:
-    /* function getPlayers() public view returns (address payable[] memory) {
-        return players;
-    } */
     function getPlayers() public view returns (address[] memory) {
         return players;
     }
