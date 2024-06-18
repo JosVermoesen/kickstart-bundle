446548ba12b2d04d5ce1a0c35b6162674a4b358e
Jos Vermoesen <JosVermoesen@users.noreply.github.com>
2024-06-18 07:31:13 +0200

    Update pollcontract.test.js

--- test/pollcontract.test.js
+++ test/pollcontract.test.js
@@ -28,81 +28,54 @@ describe("PollContract", () => {
     assert.ok(pollContract.options.address);
   });
 
-  /* it('allows one account to enter', async () => {
-    await lottery.methods.enter().send({
-      from: accounts[0],
-      value: web3.utils.toWei('0.011', 'ether'),
-    });
-
-    const players = await lottery.methods.getPlayers().call({
-      from: accounts[0],
-    });
-
-    assert.equal(accounts[0], players[0]);
-    assert.equal(1, players.length);
-  });
-
-  it('allows multiple accounts to enter', async () => {
-    await lottery.methods.enter().send({
-      from: accounts[0],
-      value: web3.utils.toWei('0.011', 'ether'),
-    });
-
-    await lottery.methods.enter().send({
-      from: accounts[1],
-      value: web3.utils.toWei('0.011', 'ether'),
-    });
-
-    await lottery.methods.enter().send({
-      from: accounts[2],
-      value: web3.utils.toWei('0.011', 'ether'),
-    });
-
-    const players = await lottery.methods.getPlayers().call({
-      from: accounts[0],
-    });
-
-    assert.equal(accounts[0], players[0]);
-    assert.equal(accounts[1], players[1]);
-    assert.equal(accounts[2], players[2]);
-    assert.equal(3, players.length);
-  });
-
-  it('requires a minimum amount of ether to enter', async () => {
-    try {
-      await lottery.methods.enter().send({
+  /* it("only owner can create polls", async () => {
+    let resultCreate = await pollContract.methods
+      .createPoll(
+        "Do you like dogs or cats?",
+        "https://blockchain-poll.vsoft.be/images/Dog-and-cat.jpg",
+        ["Cats", "Dogs", "None"].map(web3.utils.utf8ToHex)
+      )
+      .send({
         from: accounts[0],
-        value: 0,
+        gas: "1000000",
       });
-      assert(false);
-    } catch (err) {
-      assert(err);
-    }
-  });
+    // console.log("createPollResult:", "\n", resultCreate);
 
-  it('only manager can call pickWinner', async () => {
-    try {
-      await lottery.methods.pickWinner().send({
-        from: accounts[1],
+    let createdPoll = await pollContract.methods.getPoll(0).call({
+      from: accounts[1],
+    });    
+    assert.equal(createdPoll[1], "Do you like dogs or cats?");
+    // console.log("createdPoll:", "\n", createdPoll, "\n");
+
+    resultCreate = await pollContract.methods
+      .createPoll(
+        "Preferred breakfast?",
+        "https://blockchain-poll.vsoft.be/images/breakfast.jpg",
+        ["English", "Continental", "Full Scottish"].map(web3.utils.utf8ToHex)
+      )
+      .send({
+        from: accounts[0],
+        gas: "1000000",
       });
-      assert(false);
-    } catch (err) {
-      assert(err);
-    }
-  });
+    // console.log("createPollResult:", "\n", resultCreate);
 
-  it('sends money to the winner and resets the players array', async () => {
-    await lottery.methods.enter().send({
-      from: accounts[0],
-      value: web3.utils.toWei('2', 'ether'),
+    createdPoll = await pollContract.methods.getPoll(1).call({
+      from: accounts[1],
     });
+    assert.equal(createdPoll[1], "Preferred breakfast?");
+    // console.log("createdPoll:", "\n", createdPoll, "\n");
+    
+    let voteResult = await pollContract.methods
+      .vote(0, 2)
+      .send({ from: accounts[1], gas: "1000000" });
+    // console.log("voteResult0:", "\n", voteResult, "\n");
 
-    const initialBalance = await web3.eth.getBalance(accounts[0]);
-    await lottery.methods.pickWinner().send({
-      from: accounts[0],
-    });
-    const finalBalance = await web3.eth.getBalance(accounts[0]);
-    const difference = finalBalance - initialBalance;
-    assert(difference > web3.utils.toWei('1.8', 'ether'));
+    voteResult = await pollContract.methods
+      .vote(1, 0)
+      .send({ from: accounts[1], gas: "1000000" });
+    // console.log("voteResult1:", "\n", voteResult, "\n");
+
+    totalPolls = await pollContract.methods.getTotalPolls().call();
+    assert.equal(totalPolls, 2);
   }); */
 });
